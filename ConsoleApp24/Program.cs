c31bd958f30a730162cba460bb905e749b74c345
MetinAgayev <matinga@code.edu.az>
2021-12-11 23:57:59 +0400

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Metin", 2, "Muhendis", "UNEC", "metin@mail.ru", 55465145);
+            Console.WriteLine(s.StudentMelumatlari());
+        }
+    }
+}
---
+++ ConsoleApp24/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp24
+{
+    class Student
+    {
+        public string FullName;
+        public int Course;
+        public string Subject;
+        public string University;
+        public string Email;
+        public int PhoneNumber;
+        public Student(string fullname, int course, string subject, string university, string email, int phoneNumber)
+        {
+            FullName = fullname;
+            Course = course;
+            Subject = subject;
+            University = university;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+
+        public string StudentMelumatlari()
+        {
+            return $"{FullName} {Course} {Subject} {University} {Email} {PhoneNumber}";
+
+        }
+    }
+}
